26abe8c75a7b79cd7878d4dba677811058c85033
harsh170699 <harsh.shah11@einfochips.com>
2025-05-02 16:00:19 +0530

    3. React components

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,23 @@ function App() {
   return (
     // you can only return one element, so you can use a fragment or a div
     <div>
-      <div>
-        <img src="ageOfUltron.jpg" alt='ageOfUltron.jpg' width="20%" height="20%"/>
-      </div>
-      <h2>Name: Marvel: Age of Ultron</h2>
-      <h3>Rating: 5.8</h3>
-      <p>Summary: Tony Stark builds an artificial intelligence system named Ultron with the help of Bruce Banner. When the sentient Ultron makes plans to wipe out the human race, the Avengers set out to stop him.</p>
+      <NetflixSeries />
+      <NetflixSeries />
     </div>
   )
 }
 
+// React component
+const NetflixSeries = () => {
+  return (
+  <div>
+    <div>
+      <img src="ageOfUltron.jpg" alt='ageOfUltron.jpg' width="20%" height="20%" />
+    </div>
+    <h2>Name: Marvel: Age of Ultron</h2>
+    <h3>Rating: 5.8</h3>
+    <p>Summary: Tony Stark builds an artificial intelligence system named Ultron with the help of Bruce Banner. When the sentient Ultron makes plans to wipe out the human race, the Avengers set out to stop him.</p>
+  </div>
+  );
+}
 export default App
