d4c61cbcf8b71c5d1fe09ef456a0c0bbc8845ea1
harsh170699 <harsh.shah11@einfochips.com>
2025-05-05 16:42:44 +0530

    6. Conditional Values in JSX

--- src/App.jsx
+++ src/App.jsx
@@ -42,11 +42,16 @@ function App() {
 const NetflixSeries = () => {
   const name = "Avengers: Age of Ultron";
   const rating = "5.8";
+  let age = 16;
   const summary = "Tony Stark builds an artificial intelligence system named Ultron with the help of Bruce Banner. When the sentient Ultron makes plans to wipe";
   const returnGenre = () => {
     const genre = "RomCom";
     return genre;
   };
+
+  const canWatch = () => {
+    return age >= 18 ? "Watch Now" : "Not Available";
+  }
   return (
   <div>
     <div>
@@ -56,6 +61,8 @@ const NetflixSeries = () => {
     <h3>Rating: {10 + 3.2}</h3>
     <p>Summary: {summary}</p>
     <p>Genre: {returnGenre()}</p>
+    {/* <button>{age >= 18 ? "Watch Now" : "Not Available"}</button> */}
+    <button>{canWatch()}</button>
   </div>
   );
 }
