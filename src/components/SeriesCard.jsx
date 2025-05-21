fcb0ba6b762ce6effbec989f699ea769db6a49fd
harsh170699 <harsh.shah11@einfochips.com>
2025-05-21 19:08:15 +0530

    16. Styled Components in React JS

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -1,8 +1,10 @@
 import styles from './Netflix.module.css';
+import styled from 'styled-components';
 
 export const SeriesCard = ({ data }) => {
     const { img_url, name, rating, description, genre, cast, watch_url } = data;
-    const btn_style = {
+    const btn_style = { }
+    const UsingStyledObject = styled.button({
         padding: "1.2rem 2.4rem",
         border: "none",
         fontSize: "1.6rem",
@@ -10,7 +12,22 @@ export const SeriesCard = ({ data }) => {
         color: "var(--bg-color)",
         fontWeight: "bold",
         cursor: "pointer"
-    }
+    });
+
+    const UsingTemplateLieral = styled.button`
+        padding: 1.2rem 2.4rem;
+        border: none;
+        font-size: 1.6rem;
+        background-color: ${(props) => props.rating >= 8.5 ? "#7dcea0" : "#f7dc6f"};
+        color: var(--bg-color);
+        font-weight: bold;
+        cursor: pointer
+    `;
+    const Rating = styled.h3`
+        font-size: 1.6rem;
+        color: #7dcea0;
+        text-transform: capitalize
+    `;
     const ratingClass = rating >= 8.5 ? styles.super_hit : styles.average;
     return (
         <li className={styles.card}>
@@ -19,14 +36,16 @@ export const SeriesCard = ({ data }) => {
             </div>
             <div className={styles["card-content"]}>
                 <h2>Name: {name}</h2>
-                <h3>
+                <Rating>
                     Rating: <span className={`${styles.rating} ${ratingClass}`}>{rating}</span>
-                </h3>
+                </Rating>
                 <p>Summary: {description}</p>
                 <p>Genre: {genre}</p>
                 <p>Cast: {cast}</p>
                 <a href={watch_url} target="_blank">
-                    <button style={btn_style}>Watch Now</button>
+                    {/* <button style={btn_style}>Watch Now</button> */}
+                    {/* <UsingStyledObject>Watch Now</UsingStyledObject> */}
+                    <UsingTemplateLieral rating={rating}>Watch Now</UsingTemplateLieral>
                 </a>
             </div>
         </li>
