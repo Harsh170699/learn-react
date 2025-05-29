0acddf7019ecd46b7c0d50fc6c2d46ec1502e52a
harsh170699 <harsh.shah11@einfochips.com>
2025-05-29 20:06:23 +0530

    22. How React States Work: With theory, Animation and Examples

--- src/components/hooks/State.jsx
+++ src/components/hooks/State.jsx
@@ -2,15 +2,35 @@ import { useState } from "react";
 
 export const State = () => {
     const [count, setCount] = useState(0);
+    console.log("Parent component rendered");
     const handleButtonClick = () => {
         setCount(() => count + 1);
     }
     return (
         <>
-            <section className="main-div">
+            <div className="main-div">
                 <h1>{count}</h1>
                 <button onClick={handleButtonClick}>Increment</button>
-            </section>
+            </div>
+            <ChildComponent count={count} />
         </>
+    );
+}
+
+function ChildComponent({count}) {
+    console.log("Child component rendered");
+    return (
+        <div className="main-div">
+            <h2>Child Component - {count}</h2>
+        </div>
+    );
+}
+
+export function SiblingComponent() {
+    console.log("Sibling component rendered");
+    return (
+        <div className="main-div">
+            <h2>Shibling Child Component</h2>
+        </div>
     );
 }
