9bff2c3db2d272e38621927c2cb9fb54d5137932
harsh170699 <harsh.shah11@einfochips.com>
2025-06-12 19:27:48 +0530

    27. Best Website to Find Icons for Your React App

--- src/components/projects/ToggleSwitch/ToggleSwitch.jsx
+++ src/components/projects/ToggleSwitch/ToggleSwitch.jsx
@@ -1,17 +1,26 @@
 import { useState } from 'react';
 import './ToggleSwitch.css';
+import { IoIosSwitch } from "react-icons/io";
+import { PiUserSwitchLight } from "react-icons/pi";
+
 export const ToggleSwitch = () => {
     const [isOn, setIsOn] = useState(false);
     const handleToggleSwitch = () => {
         setIsOn(!isOn);
     }
     const checkIsOn = isOn ? 'on' : 'off';
-    const toggleBgColor = {backgroundColor: isOn ? '#4caf50' : '#f44336'}
+    const toggleBgColor = { backgroundColor: isOn ? '#4caf50' : '#f44336' }
     return (
-        <div className="toggle-switch" onClick={handleToggleSwitch} style={toggleBgColor}>
-            <div className={`switch ${checkIsOn}`}>
-                <span className="switch-state">{checkIsOn}</span>
+        <>
+            <h1 style={{color: '#000', textAlign: 'center'}}>
+                Toggle Switch {' '}<IoIosSwitch style={{color: 'red', textAlign: 'center'}}/>
+                <PiUserSwitchLight />
+            </h1>
+            <div className="toggle-switch" onClick={handleToggleSwitch} style={toggleBgColor}>
+                <div className={`switch ${checkIsOn}`}>
+                    <span className="switch-state">{checkIsOn}</span>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
